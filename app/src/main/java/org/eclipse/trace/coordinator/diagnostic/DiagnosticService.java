fa13be5d2a3bd2742012cfec358e2d624b7b0da7
Spiritus2424 <46603536+Spiritus2424@users.noreply.github.com>
2022-12-17 13:45:13 -0500

    Refactor Diagnostic API (#13)

--- app/src/main/java/org/eclipse/trace/coordinator/diagnostic/DiagnosticService.java
+++ app/src/main/java/org/eclipse/trace/coordinator/diagnostic/DiagnosticService.java
@@ -1,37 +1,22 @@
 package org.eclipse.trace.coordinator.diagnostic;
 
-import org.eclipse.trace.coordinator.configuration.ConfigurationService;
 import org.eclipse.trace.coordinator.traceserver.TraceServer;
 import org.eclipse.tsp.java.client.models.health.Health;
 import org.eclipse.tsp.java.client.models.health.HealthStatus;
 import org.eclipse.tsp.java.client.protocol.restclient.TspClientResponse;
-import org.eclipse.tsp.java.client.protocol.tspclient.TspClient;
 
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Inject;
 
 @ApplicationScoped
 public class DiagnosticService {
-    private Health status;
 
-    @Inject
-    ConfigurationService configurationService;
-
-    DiagnosticService() {
-        status = new Health(HealthStatus.UP);
-    }
-
-    public Health getStatus() {
-        for (TraceServer traceServer : configurationService.getConfiguration().getTraceServers()) {
-            TspClient tspClient = new TspClient(traceServer.getUrlWithPort());
-            TspClientResponse<Health> response = tspClient.checkHealth();
-
-            if (response.getResponseModel().getStatus() == HealthStatus.DOWN) {
-                status.setStatus(HealthStatus.DOWN);
-                break;
-            }
+    public Health getStatus(TraceServer traceServer) {
+        TspClientResponse<Health> response = traceServer.getTspClient().checkHealth();
+        Health health = new Health(HealthStatus.UP);
+        if (response.getResponseModel().getStatus() == HealthStatus.DOWN) {
+            health.setStatus(HealthStatus.DOWN);
         }
 
-        return status;
+        return health;
     }
 }
