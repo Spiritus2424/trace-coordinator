e26f967ad55bff7457be66cfb3ade014f2c9710e
Ahmad Faour <98.ahmad.faour@gmail.com>
2023-02-02 21:09:00 +0000

    add PostConstruct method

--- app/src/main/java/org/eclipse/trace/coordinator/experiment/DistributedExperimentManager.java
+++ app/src/main/java/org/eclipse/trace/coordinator/experiment/DistributedExperimentManager.java
@@ -1,15 +1,43 @@
 package org.eclipse.trace.coordinator.experiment;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
+import org.eclipse.trace.coordinator.configuration.Configuration;
+import org.eclipse.trace.coordinator.experiment.properties.ExperimentProperties;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+
+import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class DistributedExperimentManager {
 
     private Map<UUID, DistributedExperiment> distributedExperiments;
+
+    @PostConstruct
+    public void loadDistributedExperiment() {
+        String fileName = System.getProperty("TRACE_COORDINATOR_FILE");
+        File file = new File(fileName != null ? fileName : ".trace-coordinator.yml");
+        ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
+
+        try {
+            List<ExperimentProperties> listOfExperimentProperties = objectMapper
+                    .readValue(file, Configuration.class).getExperimentProperties();
+            for (ExperimentProperties experimentProperties : listOfExperimentProperties) {
+                DistributedExperiment distributedExperiment = experimentProperties.toObject();
+                this.distributedExperiments.put(distributedExperiment.getUuid(), distributedExperiment);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public DistributedExperimentManager() {
         this.distributedExperiments = new HashMap<>();
