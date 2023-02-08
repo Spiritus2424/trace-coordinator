b5227cbc604814c3556b75a1a13449ad0e83ad58
Ahmad Faour <98.ahmad.faour@gmail.com>
2023-02-08 23:36:23 +0000

    on standby

--- app/src/main/java/org/eclipse/trace/coordinator/experiment/DistributedExperiment.java
+++ app/src/main/java/org/eclipse/trace/coordinator/experiment/DistributedExperiment.java
@@ -12,24 +12,26 @@ import org.eclipse.tsp.java.client.models.trace.Trace;
 public class DistributedExperiment {
     private UUID uuid;
     private String name;
-    private List<Experiment> experiments;
+    private List<String> tracesUuid;
 
     public DistributedExperiment() {
+        this.name = "";
         this.uuid = UUID.randomUUID();
-        this.experiments = new ArrayList<>();
+        this.tracesUuid = new ArrayList<>();
     }
 
     public DistributedExperiment(String name) {
         this.name = name;
         this.uuid = UUID.randomUUID();
-        this.experiments = new ArrayList<>();
+        this.tracesUuid = new ArrayList<>();
     }
 
-    public DistributedExperiment(String name, List<Experiment> experiments) {
+    public DistributedExperiment(String name, List<String> tracesUuid) {
         this.name = name;
-        this.experiments = experiments;
-        this.uuid = UUID.fromString(experiments.stream().map(experiment -> experiment.getUuid()).reduce("",
-                (String partialUuid, String experimentUuid) -> partialUuid.concat(experimentUuid)));
+        this.tracesUuid = tracesUuid;
+        this.uuid = UUID.fromString(tracesUuid.stream()
+                .map(traceUuid -> traceUuid.toString())
+                .reduce("", (String partialUuid, String traceUuid) -> partialUuid.concat(traceUuid)));
 
     }
 
@@ -45,64 +47,65 @@ public class DistributedExperiment {
         this.name = name;
     }
 
-    public BigInteger getStart() {
-        BigInteger start = experiments.get(0).getStart();
+    public List<String> getTracesUuid() {
+        return tracesUuid;
+    }
 
-        for (Experiment experiment : experiments) {
-            if (start.compareTo(experiment.getStart()) == 1) {
-                start = experiment.getStart();
-            }
-        }
-        return start;
+    public BigInteger getStart() {
+        throw new UnsupportedOperationException();
+        // BigInteger start = experiments.get(0).getStart();
+
+        // for (Experiment experiment : experiments) {
+        // if (start.compareTo(experiment.getStart()) == 1) {
+        // start = experiment.getStart();
+        // }
+        // }
+        // return start;
     }
 
     public BigInteger getEnd() {
-        BigInteger end = experiments.get(0).getStart();
+        throw new UnsupportedOperationException();
+        // BigInteger end = experiments.get(0).getStart();
 
-        for (Experiment experiment : experiments) {
-            if (end.compareTo(experiment.getStart()) == -1) {
-                end = experiment.getStart();
-            }
-        }
-        return end;
+        // for (Experiment experiment : experiments) {
+        // if (end.compareTo(experiment.getStart()) == -1) {
+        // end = experiment.getStart();
+        // }
+        // }
+        // return end;
     }
 
     public int getNbEvents() {
-        int nbEvents = 0;
+        throw new UnsupportedOperationException();
+        // int nbEvents = 0;
 
-        for (Experiment experiment : experiments) {
-            nbEvents += experiment.getNbEvents();
-        }
-        return nbEvents;
+        // for (Experiment experiment : experiments) {
+        // nbEvents += experiment.getNbEvents();
+        // }
+        // return nbEvents;
     }
 
     public IndexingStatus getIndexingStatus() {
-        IndexingStatus indexingStatus = null;
-        for (Experiment experiment : experiments) {
-            if (experiment.getIndexingStatus() == IndexingStatus.CLOSED) {
-                indexingStatus = IndexingStatus.CLOSED;
-                break;
-            }
-        }
+        throw new UnsupportedOperationException();
+        // IndexingStatus indexingStatus = null;
+        // for (Experiment experiment : experiments) {
+        // if (experiment.getIndexingStatus() == IndexingStatus.CLOSED) {
+        // indexingStatus = IndexingStatus.CLOSED;
+        // break;
+        // }
+        // }
 
-        return indexingStatus;
+        // return indexingStatus;
     }
 
     public List<Trace> getTraces() {
-        List<Trace> traces = new ArrayList<>();
-        for (Experiment experiment : experiments) {
-            traces.addAll(experiment.getTraces());
-        }
+        throw new UnsupportedOperationException();
+        // List<Trace> traces = new ArrayList<>();
+        // for (Experiment experiment : experiments) {
+        // traces.addAll(experiment.getTraces());
+        // }
 
-        return traces;
-    }
-
-    public List<Experiment> getExperiments() {
-        return experiments;
-    }
-
-    public boolean addExperiment(Experiment experiment) {
-        return this.experiments.add(experiment);
+        // return traces;
     }
 
     public Experiment toExperiment() {
