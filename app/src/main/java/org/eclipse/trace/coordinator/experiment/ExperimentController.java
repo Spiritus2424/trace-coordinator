98b275f9aa7097f2b9c6cf80664fdbe6123176e3
Ahmad Faour <98.ahmad.faour@gmail.com>
2023-02-08 23:39:45 +0000

    Implement bare metal experiment controller without manager

--- app/src/main/java/org/eclipse/trace/coordinator/experiment/ExperimentController.java
+++ app/src/main/java/org/eclipse/trace/coordinator/experiment/ExperimentController.java
@@ -1,14 +1,19 @@
 package org.eclipse.trace.coordinator.experiment;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
+import org.eclipse.trace.coordinator.trace.TraceService;
 import org.eclipse.trace.coordinator.traceserver.TraceServer;
 import org.eclipse.trace.coordinator.traceserver.TraceServerManager;
 import org.eclipse.tsp.java.client.models.experiment.Experiment;
 import org.eclipse.tsp.java.client.models.query.Query;
+import org.eclipse.tsp.java.client.models.trace.Trace;
 
+import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.validation.constraints.NotNull;
@@ -32,20 +37,39 @@ public class ExperimentController {
     @Inject
     ExperimentService experimentService;
 
-    @Inject
-    DistributedExperimentManager distributedExperimentManager;
+    // @Inject
+    // DistributedExperimentManager distributedExperimentManager;
 
     @Inject
     TraceServerManager traceServerManager;
+
+    @Inject
+    TraceService traceService;
+
+    @PostConstruct
+    public void createExperiments() {
+        // for (DistributedExperiment distributedExperiment :
+        // distributedExperimentManager.getDistributedExperiments()
+        // .values()) {
+        // for (TraceServer traceServer : traceServerManager.getTraceServers()) {
+        // Map<String, Object> parameters = new HashMap<>();
+        // parameters.put("name", traceServer.getHost());
+        // parameters.put("traces", distributedExperiment.getTracesUuid());
+        // experimentService.createExperiment(traceServer, new Query(parameters));
+        // }
+
+        // }
+
+    }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     public Response getExperiments() {
         List<Experiment> experiments = new ArrayList<>();
-        for (DistributedExperiment distributedExperiment : this.distributedExperimentManager.getDistributedExperiments()
-                .values()) {
-            experiments.add(distributedExperiment.toExperiment());
+
+        for (TraceServer traceServer : this.traceServerManager.getTraceServers()) {
+            experiments.addAll(this.experimentService.getExperiments(traceServer));
         }
 
         return Response.ok(experiments).build();
@@ -58,12 +82,14 @@ public class ExperimentController {
     public Response getExperiment(@NotNull @PathParam("expUUID") UUID experimentUuid) {
         Response response = null;
 
-        if (this.distributedExperimentManager.hasDistributedExperiment(experimentUuid)) {
-            response = Response
-                    .ok(this.distributedExperimentManager.getDistributedExperiment(experimentUuid).toExperiment())
-                    .build();
-        } else {
-            response = Response.status(Status.NOT_FOUND).entity("No Such Experiment").build();
+        for (TraceServer traceServer : traceServerManager.getTraceServers()) {
+            Experiment experiment = experimentService.getExperiment(traceServer, experimentUuid.toString());
+            if (experiment != null) {
+                response = Response.ok(experiment).build();
+                break;
+            } else {
+                response = Response.status(Status.NOT_FOUND).entity("No Such Trace").build();
+            }
         }
 
         return response;
@@ -72,28 +98,30 @@ public class ExperimentController {
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
+    @SuppressWarnings("unchecked")
     public Response createExperiment(@NotNull Query query) {
-        Response response = null;
+        List<Experiment> experiments = new ArrayList<>();
 
-        DistributedExperiment distributedExperiment = null;
-        if (query.getParameters().containsKey("name") && query.getParameters().containsKey("traces")) {
-            distributedExperiment = new DistributedExperiment(query.getParameters().get("name").toString());
-
-            for (TraceServer traceServer : this.traceServerManager.getTraceServers()) {
-                query.getParameters().put("name",
-                        String.format("%s$%s", traceServer.getHost(), distributedExperiment.getName()));
-                Experiment experiment = this.experimentService.createExperiment(traceServer, query);
-                if (experiment != null) {
-                    distributedExperiment.addExperiment(experiment);
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("name", query.getParameters().get("name"));
+        for (TraceServer traceServer : traceServerManager.getTraceServers()) {
+            List<Trace> traces = traceService.getTraces(traceServer);
+            List<String> traceServerTracesUuid = new ArrayList<>();
+            for (Trace trace : traces) {
+                for (String traceUuid : (ArrayList<String>) query.getParameters().get("traces")) {
+                    if (trace.getUuid().equals(traceUuid)) {
+                        traceServerTracesUuid.add(traceUuid);
+                    }
                 }
             }
-            this.distributedExperimentManager.addDistributedExperiment(distributedExperiment);
-            response = Response.ok(distributedExperiment.toExperiment()).build();
-        } else {
-            response = Response.status(Status.BAD_REQUEST).entity("Missing name and traces body parameter").build();
+
+            if (traceServerTracesUuid.size() > 0) {
+                parameters.put("traces", traceServerTracesUuid);
+                experiments.add(experimentService.createExperiment(traceServer, new Query(parameters)));
+            }
         }
 
-        return response;
+        return Response.ok(experiments).build();
     }
 
     @PUT
@@ -110,20 +138,17 @@ public class ExperimentController {
     @Consumes(MediaType.APPLICATION_JSON)
     @Path("{expUUID}")
     public Response deleteExperiment(@NotNull @PathParam("expUUID") UUID experimentUuid) {
+        List<Experiment> experiments = new ArrayList<>();
+        for (TraceServer traceServer : traceServerManager.getTraceServers()) {
+            experiments.add(experimentService.deleteExperiment(traceServer, experimentUuid.toString()));
+        }
+
         Response response = null;
-
-        if (this.distributedExperimentManager.hasDistributedExperiment(experimentUuid)) {
-            DistributedExperiment distributedExperiment = this.distributedExperimentManager
-                    .removeDistributedExperiment(experimentUuid);
-
-            for (TraceServer traceServer : this.traceServerManager.getTraceServers()) {
-                for (Experiment experiment : distributedExperiment.getExperiments()) {
-                    this.experimentService.deleteExperiment(traceServer, experiment.getUuid());
-                }
-            }
-
+        if (experiments.size() != 0) {
+            response = Response.ok(experiments).build();
         } else {
             response = Response.status(Status.NOT_FOUND).entity("No Such Experiment").build();
+
         }
 
         return response;
