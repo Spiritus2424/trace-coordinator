ad34745f2ddfff04c2869712f8c4ca244fc63a52
Ahmad Faour <98.ahmad.faour@gmail.com>
2023-02-25 04:47:02 +0000

    implemented GET route

--- app/src/main/java/org/eclipse/trace/coordinator/annotation/AnnotationController.java
+++ app/src/main/java/org/eclipse/trace/coordinator/annotation/AnnotationController.java
@@ -1,14 +1,28 @@
 package org.eclipse.trace.coordinator.annotation;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
+
+import org.eclipse.trace.coordinator.traceserver.TraceServer;
+import org.eclipse.trace.coordinator.traceserver.TraceServerManager;
+import org.eclipse.tsp.java.client.models.annotation.AnnotationCategoriesModel;
+import org.eclipse.tsp.java.client.models.query.Query;
+import org.eclipse.tsp.java.client.models.response.GenericResponse;
+import org.eclipse.tsp.java.client.models.response.ResponseStatus;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import jakarta.validation.constraints.NotNull;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
@@ -17,20 +31,43 @@ import jakarta.ws.rs.core.Response;
 public class AnnotationController {
 
     @Inject
+    TraceServerManager traceServerManager;
+
+    @Inject
     private AnnotationService annotationService;
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response getAnnotationCategories(UUID experimentUuid, UUID outputId) {
+    public Response getAnnotationCategories(@PathParam("expUUID") UUID experimentUuid,
+            @PathParam("outputId") String outputId,
+            @QueryParam("markerSetId") String markerSetId) {
+        Set<String> annotationCategories = new HashSet<>();
+        ResponseStatus responseStatus = ResponseStatus.COMPLETED;
+        String statusMessage = null;
+        for (TraceServer traceServer : traceServerManager.getTraceServers()) {
+            GenericResponse<AnnotationCategoriesModel> genericResponse = this.annotationService
+                    .getAnnotationCategories(traceServer, experimentUuid, outputId,
+                            markerSetId != null ? Optional.of(markerSetId) : Optional.empty());
+            if (responseStatus != ResponseStatus.RUNNING) {
+                responseStatus = genericResponse.getStatus();
+                statusMessage = genericResponse.getMessage();
+            }
+            if (genericResponse.getModel() != null) {
+                annotationCategories.addAll(genericResponse.getModel().getAnnotationCategories());
+            }
+        }
 
-        return Response.ok().build();
+        return Response.ok(new GenericResponse<AnnotationCategoriesModel>(
+                new AnnotationCategoriesModel(new ArrayList<>(annotationCategories)), responseStatus, statusMessage))
+                .build();
     }
 
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response getAnnotation(UUID experimentUuid, UUID outputId) {
+    public Response getAnnotation(@PathParam("expUUID") UUID experimentUuid,
+            @PathParam("outputId") String outputId, @NotNull Query query) {
 
         return Response.ok().build();
     }
--- app/src/main/java/org/eclipse/trace/coordinator/annotation/AnnotationService.java
+++ app/src/main/java/org/eclipse/trace/coordinator/annotation/AnnotationService.java
@@ -1,8 +1,21 @@
 package org.eclipse.trace.coordinator.annotation;
 
+import java.util.Optional;
+import java.util.UUID;
+
+import org.eclipse.trace.coordinator.traceserver.TraceServer;
+import org.eclipse.tsp.java.client.models.annotation.AnnotationCategoriesModel;
+import org.eclipse.tsp.java.client.models.response.GenericResponse;
+
 import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class AnnotationService {
 
+    public GenericResponse<AnnotationCategoriesModel> getAnnotationCategories(TraceServer traceServer,
+            UUID experimentUuid, String outputId,
+            Optional<String> markerSetId) {
+        return traceServer.getTspClient().getAnnotationsCategories(experimentUuid, outputId, markerSetId)
+                .getResponseModel();
+    }
 }
