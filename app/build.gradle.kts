ddefd0026aaccf8e47c9ece4113909d8928a0184
Ahmad Faour <98.ahmad.faour@gmail.com>
2023-03-08 23:18:50 +0000

    update to java 17 and upgrade to insiders tsp-java-client library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,6 +15,12 @@ plugins {
     `maven-publish`
 }
 
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
 repositories {
     mavenCentral()
     mavenLocal()
@@ -29,6 +35,7 @@ repositories {
     }
 }
 
+
 dependencies {
     // Use JUnit Jupiter for testing.
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
@@ -36,7 +43,7 @@ dependencies {
     // This dependency is used by the application.
     providedCompile("jakarta.platform:jakarta.jakartaee-api:10.0.0")
     
-    implementation("org.eclipse:tsp-java-client:v1.2.1-alpha")
+    implementation("org.eclipse:tsp-java-client-insiders:v1.2.2-alpha")
     
     implementation("com.fasterxml.jackson.core:jackson-databind:2.14.1")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.14.1")
