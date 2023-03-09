71477c6a445f055925ff9fcb4715c63900d830a4
Ahmad Faour <98.ahmad.faour@gmail.com>
2023-03-09 22:22:16 +0000

    add filter api

---
+++ app/src/main/java/org/eclipse/trace/coordinator/filter/FilterController.java
@@ -0,0 +1,68 @@
+package org.eclipse.trace.coordinator.filter;
+
+import org.eclipse.trace.coordinator.traceserver.TraceServerManager;
+import org.eclipse.tsp.java.client.models.filter.Filter;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
+
+@Path("filters")
+@ApplicationScoped
+public class FilterController {
+
+    @Inject
+    private FilterService filterService;
+
+    @Inject
+    private TraceServerManager traceServerManager;
+
+    @GET
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getFilters() {
+        return Response.status(Status.NOT_IMPLEMENTED).build();
+    }
+
+    @GET
+    @Path("{filterId}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getFilter(@PathParam("filterId") String filterId) {
+        return Response.status(Status.NOT_IMPLEMENTED).build();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response createFilter(Filter Filter) {
+        return Response.status(Status.NOT_IMPLEMENTED).build();
+    }
+
+    @PUT
+    @Path("{filterId}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response updateFilter(@PathParam("filterId") String filterId, Filter filter) {
+        return Response.status(Status.NOT_IMPLEMENTED).build();
+    }
+
+    @DELETE
+    @Path("{filterId}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response deleteFilter(@PathParam("filterId") String filterId) {
+        return Response.status(Status.NOT_IMPLEMENTED).build();
+    }
+
+}
---
+++ app/src/main/java/org/eclipse/trace/coordinator/filter/FilterService.java
@@ -0,0 +1,33 @@
+package org.eclipse.trace.coordinator.filter;
+
+import java.util.List;
+
+import org.eclipse.trace.coordinator.traceserver.TraceServer;
+import org.eclipse.tsp.java.client.models.bookmark.Bookmark;
+import org.eclipse.tsp.java.client.models.filter.Filter;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class FilterService {
+
+    public List<Filter> getFilters(TraceServer traceServer) {
+        throw new UnsupportedOperationException("Not Implemented");
+    }
+
+    public Filter getFilter(TraceServer traceServer, String filterId) {
+        throw new UnsupportedOperationException("Not Implemented");
+    }
+
+    public Filter createFilter(TraceServer traceServer, Bookmark bookmark) {
+        throw new UnsupportedOperationException("Not Implemented");
+    }
+
+    public Filter updateFilter(TraceServer traceServer, String filterId, Filter filter) {
+        throw new UnsupportedOperationException("Not Implemented");
+    }
+
+    public Filter deleteFilter(TraceServer traceServer, String filterId) {
+        throw new UnsupportedOperationException("Not Implemented");
+    }
+}
