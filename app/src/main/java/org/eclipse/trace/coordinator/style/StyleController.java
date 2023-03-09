53b3a9df4ccedcc64a39749a09396c78b1b92a5c
Ahmad Faour <98.ahmad.faour@gmail.com>
2023-03-09 20:53:11 +0000

    create controller and service of style api

---
+++ app/src/main/java/org/eclipse/trace/coordinator/style/StyleController.java
@@ -0,0 +1,13 @@
+package org.eclipse.trace.coordinator.style;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.Path;
+
+/**
+ * StyleController
+ */
+@Path("")
+@ApplicationScoped
+public class StyleController {
+
+}
---
+++ app/src/main/java/org/eclipse/trace/coordinator/style/StyleService.java
@@ -0,0 +1,8 @@
+package org.eclipse.trace.coordinator.style;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class StyleService {
+
+}
