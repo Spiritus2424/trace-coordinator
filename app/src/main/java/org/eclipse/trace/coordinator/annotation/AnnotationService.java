b3ce8e341bafbe1bc6e338dff3157e697259a328
Ahmad Faour <98.ahmad.faour@gmail.com>
2023-04-18 03:18:10 +0000

    add Async api

--- app/src/main/java/org/eclipse/trace/coordinator/annotation/AnnotationService.java
+++ app/src/main/java/org/eclipse/trace/coordinator/annotation/AnnotationService.java
@@ -3,6 +3,7 @@ package org.eclipse.trace.coordinator.annotation;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 import org.eclipse.trace.coordinator.traceserver.TraceServer;
 import org.eclipse.tsp.java.client.api.annotation.AnnotationCategoriesModel;
@@ -22,6 +23,7 @@ public class AnnotationService {
     public GenericResponse<AnnotationCategoriesModel> getAnnotationCategories(TraceServer traceServer,
             UUID experimentUuid, String outputId,
             Optional<String> markerSetId) {
+
         return traceServer.getTspClient().getAnnotationApi()
                 .getAnnotationsCategories(experimentUuid, outputId, markerSetId)
                 .getResponseModel();
@@ -33,9 +35,9 @@ public class AnnotationService {
         if (query.getParameters().containsKey("requested_items")) {
             List<Integer> requested_items = (List<Integer>) query.getParameters().get("requested_items");
 
-            for (int i = 0; i < requested_items.size(); i++) {
-                requested_items.set(i, traceServer.decodeEntryId(requested_items.get(i)));
-            }
+            requested_items.parallelStream().map((Integer encodeEntryId) -> {
+                return traceServer.decodeEntryId(encodeEntryId);
+            });
 
             query.getParameters().put("requested_items", requested_items);
         }
@@ -47,5 +49,37 @@ public class AnnotationService {
         this.annotationAnalysis.computeAnnotationModel(traceServer,
                 genericResponse.getModel().getAnnotations());
         return genericResponse;
+    }
+
+    public CompletableFuture<GenericResponse<AnnotationModel>> getAnnotationModelAsync(TraceServer traceServer,
+            UUID experimentUuid, String outputId, Query query) {
+
+        if (query.getParameters().containsKey("requested_items")) {
+            List<Integer> requested_items = (List<Integer>) query.getParameters().get("requested_items");
+
+            requested_items.parallelStream().map((Integer encodeEntryId) -> {
+                return traceServer.decodeEntryId(encodeEntryId);
+            });
+
+            query.getParameters().put("requested_items", requested_items);
+        }
+
+        return traceServer.getTspClient()
+                .getAnnotationApiAsync()
+                .getAnnotations(experimentUuid, outputId, query).thenApply((result) -> {
+                    this.annotationAnalysis.computeAnnotationModel(traceServer,
+                            result.getResponseModel().getModel().getAnnotations());
+                    return result.getResponseModel();
+                });
+    }
+
+    public CompletableFuture<GenericResponse<AnnotationCategoriesModel>> getAnnotationCategoriesAsync(
+            TraceServer traceServer,
+            UUID experimentUuid, String outputId,
+            Optional<String> markerSetId) {
+
+        return traceServer.getTspClient().getAnnotationApiAsync()
+                .getAnnotationsCategories(experimentUuid, outputId, markerSetId)
+                .thenApply(result -> result.getResponseModel());
     }
 }
