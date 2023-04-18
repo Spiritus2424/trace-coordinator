326bc16f9be64e2a67cf468c1ff4cd0dc813bae2
Ahmad Faour <98.ahmad.faour@gmail.com>
2023-04-18 07:41:05 +0000

    upgrade tsp-java-client library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,17 +37,19 @@ repositories {
 
 
 dependencies {
-    // Use JUnit Jupiter for testing.
-    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
 
-    // This dependency is used by the application.
+    // Jakarta
     providedCompile("jakarta.platform:jakarta.jakartaee-api:10.0.0")
     
-    implementation("org.eclipse:tsp-java-client-insiders:v2.0.4-alpha")
-    
+    // Tsp Java Client
+    implementation("org.eclipse:tsp-java-client-insiders:v2.0.6-alpha")
+
+    // Jackson
     implementation("com.fasterxml.jackson.core:jackson-databind:2.14.1")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.14.1")
 
+    // JUnit Jupiter
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
 }
 
 
