5db4085af85ff7051d16a83bf26ac0ea1094a2e2
Ahmad Faour <98.ahmad.faour@gmail.com>
2023-04-25 06:33:14 +0000

    implement OutputDescriptor and Style Api into async

--- app/src/main/java/org/eclipse/trace/coordinator/outputdescriptor/OutputDescriptorService.java
+++ app/src/main/java/org/eclipse/trace/coordinator/outputdescriptor/OutputDescriptorService.java
@@ -4,29 +4,29 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 import org.eclipse.trace.coordinator.traceserver.TraceServer;
 import org.eclipse.tsp.java.client.api.outputdescriptor.OutputDescriptor;
-import org.eclipse.tsp.java.client.core.tspclient.TspClientResponse;
 
 import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class OutputDescriptorService {
 
-    public List<OutputDescriptor> getOutputDescriptors(TraceServer traceServer, UUID experimentUuid) {
-        TspClientResponse<OutputDescriptor[]> response = traceServer.getTspClient().getOutputDescriptorApi()
-                .experimentOutputs(experimentUuid,
-                        Optional.empty());
+    public CompletableFuture<List<OutputDescriptor>> getOutputDescriptors(TraceServer traceServer,
+            UUID experimentUuid) {
+        return traceServer.getTspClient().getOutputDescriptorApiAsync()
+                .experimentOutputs(experimentUuid, Optional.empty()).thenApply(response -> {
+                    List<OutputDescriptor> outputDescriptors = new ArrayList<>();
+                    if (response.isOk() && response.getResponseModel() != null) {
+                        for (OutputDescriptor outputDescriptor : response.getResponseModel()) {
+                            outputDescriptors.add(outputDescriptor);
+                        }
+                    }
+                    return outputDescriptors;
+                });
 
-        List<OutputDescriptor> outputDescriptors = new ArrayList<>();
-        if (response.isOk() && response.getResponseModel() != null) {
-            for (OutputDescriptor outputDescriptor : response.getResponseModel()) {
-                outputDescriptors.add(outputDescriptor);
-            }
-        }
-
-        return outputDescriptors;
     }
 
 }
