025733d4c69ea670b2b958c629101b3df6a3e6e0
Ahmad Faour <98.ahmad.faour@gmail.com>
2023-08-31 08:28:37 +0000

    Register ImmediateFeature

--- app/src/main/java/org/eclipse/trace/coordinator/app/TraceCoordinatorResourceConfig.java
+++ app/src/main/java/org/eclipse/trace/coordinator/app/TraceCoordinatorResourceConfig.java
@@ -2,17 +2,19 @@ package org.eclipse.trace.coordinator.app;
 
 import org.eclipse.trace.coordinator.app.cors.CorsFilter;
 import org.eclipse.trace.coordinator.shared.utils.AutoScanFeature;
+import org.eclipse.trace.coordinator.shared.utils.ImmediateFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 
 import jakarta.ws.rs.ApplicationPath;
 
 @ApplicationPath("tsp/api")
 public class TraceCoordinatorResourceConfig extends ResourceConfig {
+
 	public TraceCoordinatorResourceConfig() {
 		packages("org.eclipse.trace.coordinator.api");
 		register(AutoScanFeature.class);
 		register(CorsFilter.class);
-
+		register(ImmediateFeature.class);
 	}
 
 }
