d650e068522a2d0754ff7a46d35c17e7b2aa576d
Ahmad Faour <98.ahmad.faour@gmail.com>
2023-09-04 11:14:25 +0000

    WIP

--- app/src/main/java/org/eclipse/trace/coordinator/core/timegraph/CriticalPathAction.java
+++ app/src/main/java/org/eclipse/trace/coordinator/core/timegraph/CriticalPathAction.java
@@ -17,13 +17,17 @@ import org.eclipse.trace.coordinator.core.style.CriticalPathStyle;
 import org.eclipse.trace.coordinator.core.traceserver.TraceServer;
 import org.eclipse.trace.coordinator.core.traceserver.TraceServerManager;
 import org.eclipse.tsp.java.client.api.graph.Direction;
+import org.eclipse.tsp.java.client.api.graph.HostThread;
 import org.eclipse.tsp.java.client.api.graph.TcpEventKey;
 import org.eclipse.tsp.java.client.api.graph.Vertex;
 import org.eclipse.tsp.java.client.api.graph.Worker;
 import org.eclipse.tsp.java.client.api.timegraph.TimeGraphModel;
 import org.eclipse.tsp.java.client.api.timegraph.TimeGraphState;
+import org.eclipse.tsp.java.client.api.timegraph.dto.GetTimeGraphStatesRequestDto;
 import org.eclipse.tsp.java.client.shared.query.Body;
+import org.eclipse.tsp.java.client.shared.query.QueryInterval;
 import org.eclipse.tsp.java.client.shared.query.TimeRange;
+import org.eclipse.tsp.java.client.shared.response.GenericResponse;
 
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.Multimap;
@@ -53,7 +57,14 @@ public class CriticalPathAction implements IAction<TimeGraphModel> {
 		/*
 		 * Fetch All the traceServer Vertex indexes
 		 */
-		Map<TraceServer, Map<Vertex, TcpEventKey>> indexes = this.fetchVertexIndexes(experimentUuid);
+		// Map<TraceServer, Map<Vertex, TcpEventKey>> indexesCause =
+		// this.fetchVertexIndexes(experimentUuid,
+		// Direction.CAUSE);
+		// Map<TraceServer, Map<Vertex, TcpEventKey>> indexesEffect =
+		// this.fetchVertexIndexes(experimentUuid,
+		// Direction.EFFECT);
+
+		Map<TraceServer, Map<Vertex, TcpEventKey>> indexes = this.fetchVertexIndexes(Direction.EFFECT);
 
 		/*
 		 * Build a cluster without the traceServer that initiate the analysis
@@ -66,59 +77,53 @@ public class CriticalPathAction implements IAction<TimeGraphModel> {
 		 */
 		List<TimeGraphState> networkStates = this.findStyleState(timeGraphModel, CriticalPathStyle.NETWORK);
 
-		/*
-		 * Find unmatched vertex mapped the network state for the host TraceServer
-		 */
-		Multimap<TimeGraphState, Vertex> unmatchedVertexesForEachStates = ArrayListMultimap.create();
-		List<CompletableFuture<Void>> unmatchedVertexesFutures = new ArrayList<>();
 		for (TimeGraphState timeGraphState : networkStates) {
-			unmatchedVertexesFutures.add(
-					this.getUnmatchedVertexes(timeGraphState).thenAccept(
-							unmatchedVertexes -> unmatchedVertexesForEachStates.putAll(timeGraphState,
-									unmatchedVertexes)));
-		}
-		CompletableFuture
-				.allOf(unmatchedVertexesFutures.toArray(new CompletableFuture[unmatchedVertexesFutures.size()])).join();
+			/*
+			 * Find unmatched vertex mapped the network state for the host TraceServer
+			 */
+			List<Vertex> unmatchedEffectVertexes = this.getUnmatchedVertexes(timeGraphState).join();
 
-		/*
-		 * Find the TcpEventKey for the unmatchedVertex for each NetworkState
-		 */
-		Multimap<TimeGraphState, TcpEventKey> tcpEventKeysForEachStates = ArrayListMultimap.create();
-		for (TimeGraphState networkState : unmatchedVertexesForEachStates.keySet()) {
-			List<TcpEventKey> list = this.getTcpEventKeys(indexes.get(this.hostTraceServer),
-					(List<Vertex>) unmatchedVertexesForEachStates.get(networkState));
-			tcpEventKeysForEachStates.putAll(networkState, list);
-		}
+			/*
+			 * Find the TcpEventKey for the unmatchedVertex for each NetworkState
+			 */
+			List<TcpEventKey> tcpEventKeys = this.getTcpEventKeys(indexes.get(this.hostTraceServer),
+					unmatchedEffectVertexes);
 
-		/*
-		 * Match the tcpEventKey with other traceServer using the indexes
-		 */
-		Map<TimeGraphState, Multimap<TraceServer, Vertex>> matchedVertexesForEachStates = new HashMap<>();
-		for (Entry<TimeGraphState, TcpEventKey> tcpEventKeyEntry : tcpEventKeysForEachStates.entries()) {
-			Multimap<TraceServer, Vertex> matchedVertexes = matchedVertexesForEachStates
-					.computeIfAbsent(tcpEventKeyEntry.getKey(), __ -> ArrayListMultimap.create());
-			matchedVertexes.putAll(this.getMatchedVertexes(cluster, indexes, tcpEventKeyEntry));
-		}
+			/*
+			 * Match the tcpEventKey with other traceServer using the indexes
+			 */
+			Multimap<TraceServer, Vertex> matchedVertexes = ArrayListMultimap.create();
+			for (TcpEventKey tcpEventKey : tcpEventKeys) {
+				matchedVertexes.putAll(this.getMatchedVertexes(cluster, indexes, tcpEventKey));
+			}
+			/*
+			 * Find the Workers on each TraceServer needed to complete the criticalPath
+			 */
+			Multimap<TraceServer, Worker> traceServerWorkers = this.findTraceServerWorkers(cluster, matchedVertexes);
+			System.out.println(traceServerWorkers);
 
-		/*
-		 * Find the Workers on each TraceServer needed to complete the criticalPath
-		 */
-		Map<TimeGraphState, Multimap<TraceServer, Worker>> traceServerWorkersForEachStates = new HashMap<>();
-		for (TimeGraphState timeGraphState : matchedVertexesForEachStates.keySet()) {
-			Multimap<TraceServer, Worker> matchedVertexes = traceServerWorkersForEachStates
-					.computeIfAbsent(timeGraphState, __ -> ArrayListMultimap.create());
-			matchedVertexes
-					.putAll(this.findTraceServerWorkers(cluster, matchedVertexesForEachStates.get(timeGraphState)));
-		}
+			/*
+			 * Apply Action Critical for Worker and Get TimeGraphState from outputId
+			 * CriticalPathDataProvider
+			 */
+			for (Entry<TraceServer, Worker> workerEntry : traceServerWorkers.entries()) {
+				GenericResponse<TimeGraphModel> genericResponse = this.getStates(workerEntry.getKey(),
+						workerEntry.getValue().getHostThread(),
+						new QueryInterval(timeGraphState.getStart(), timeGraphState.getEnd()));
 
-		System.out.println(traceServerWorkersForEachStates);
+				System.out.println(genericResponse);
+			}
+
+		}
 
 	}
 
-	private Map<TraceServer, Map<Vertex, TcpEventKey>> fetchVertexIndexes(UUID experimentUuid) {
+	private Map<TraceServer, Map<Vertex, TcpEventKey>> fetchVertexIndexes(Direction direction) {
 		final Map<TraceServer, Map<Vertex, TcpEventKey>> indexes = new ConcurrentHashMap<>();
 		this.traceServerManager.getTraceServers().forEach(traceServer -> {
-			this.graphService.getVertexIndexes(traceServer, experimentUuid, Optional.of(Direction.CAUSE))
+			this.graphService
+					.getVertexIndexes(traceServer, this.experimentUuid,
+							direction == null ? Optional.empty() : Optional.of(direction))
 					.thenApply(traceServerIndexes -> indexes.put(traceServer, traceServerIndexes));
 		});
 
@@ -136,7 +141,7 @@ public class CriticalPathAction implements IAction<TimeGraphModel> {
 	private CompletableFuture<List<Vertex>> getUnmatchedVertexes(final TimeGraphState networkState) {
 		Body<TimeRange> body = new Body<>(new TimeRange(networkState.getStart(), networkState.getEnd()));
 		return this.graphService.getUnmatchedVertexes(this.hostTraceServer,
-				experimentUuid, body, Optional.of(Direction.EFFECT));
+				this.experimentUuid, body, Optional.of(Direction.EFFECT));
 	}
 
 	private List<TcpEventKey> getTcpEventKeys(Map<Vertex, TcpEventKey> traceServerIndexes,
@@ -149,16 +154,16 @@ public class CriticalPathAction implements IAction<TimeGraphModel> {
 	}
 
 	private Multimap<TraceServer, Vertex> getMatchedVertexes(List<TraceServer> cluster,
-			Map<TraceServer, Map<Vertex, TcpEventKey>> indexes, Entry<TimeGraphState, TcpEventKey> tcpEventKeyEntry) {
+			Map<TraceServer, Map<Vertex, TcpEventKey>> indexes, TcpEventKey tcpEventKey) {
 		Multimap<TraceServer, Vertex> matchedVertexes = ArrayListMultimap.create();
 
-		for (TraceServer traceServer : cluster) {
+		cluster.forEach(traceServer -> {
 			for (Entry<Vertex, TcpEventKey> indexEntry : indexes.get(traceServer).entrySet()) {
-				if (indexEntry.getValue().equals(tcpEventKeyEntry.getValue())) {
+				if (indexEntry.getValue().equals(tcpEventKey)) {
 					matchedVertexes.put(traceServer, indexEntry.getKey());
 				}
 			}
-		}
+		});
 
 		return matchedVertexes;
 	}
@@ -169,7 +174,7 @@ public class CriticalPathAction implements IAction<TimeGraphModel> {
 
 		cluster.forEach(traceServer -> {
 			List<Worker> workers = matchedVertexes.get(traceServer).stream()
-					.map(vertex -> this.graphService.getWorker(traceServer, experimentUuid, vertex.getWorkerId()))
+					.map(vertex -> this.graphService.getWorker(traceServer, this.experimentUuid, vertex.getWorkerId()))
 					.map(CompletableFuture::join)
 					.distinct() // Remove duplicate Worker Id for the same TraceServer
 					.collect(Collectors.toList());
@@ -177,5 +182,20 @@ public class CriticalPathAction implements IAction<TimeGraphModel> {
 		});
 
 		return traceServerWorkers;
+	}
+
+	private GenericResponse<TimeGraphModel> getStates(TraceServer traceServer, HostThread hostThread,
+			QueryInterval queryInterval) {
+		final String threadStatusDataProviderOutputId = "org.eclipse.tracecompass.internal.analysis.os.linux.core.threadstatus.ThreadStatusDataProvider";
+		final String actionId = "FollowThreadAction";
+		Map<String, Object> inputParameters = new HashMap<>();
+		inputParameters.put("hostThread", hostThread);
+		this.timeGraphService.applyActionTooltip(traceServer, this.experimentUuid, threadStatusDataProviderOutputId,
+				actionId, new Body<Map<String, Object>>(inputParameters)).join();
+		final String criticalPathDataProviderOutputId = "org.eclipse.tracecompass.analysis.graph.core.dataprovider.CriticalPathDataProvider";
+		return this.timeGraphService.getStates(traceServer, experimentUuid, criticalPathDataProviderOutputId,
+				new Body<GetTimeGraphStatesRequestDto>(
+						new GetTimeGraphStatesRequestDto(queryInterval, new ArrayList<>())))
+				.join();
 	}
 }
